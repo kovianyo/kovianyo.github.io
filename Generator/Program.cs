4ac0ab29df42112062543b6af631b233aab019b0
kovianyo <kovianyo@gmail.com>
2023-08-26 13:06:09 +0200

    Addig Generator

---
+++ Generator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Generator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Directory.CreateDirectory("html");
+            File.WriteAllText("html/index.html", "Hello World3");
+        }
+    }
+}
